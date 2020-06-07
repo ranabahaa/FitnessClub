2c601adecfe37d7dd3ab7cb48b4db863645f0ef3
rana bahaa <ranabahaa9000@gmail.com>
2020-06-07 08:36:36 +0200

    person and trainer classes

---
+++ ApplicationLayer/PersonClass.php
@@ -0,0 +1,173 @@
+<?php
+    include'..//DataLayer/Database.php';
+    session_start();
+    //here is the global variable for the session
+    $id=$_SESSION['id'];
+
+    class person{
+        
+        function __construct(){
+            $connectdb  = connectdb::getInstance();
+            $conn = $connectdb->getConnection();
+
+        }
+
+        function login($id,$mail,$pass){
+            $connectdb  = connectdb::getInstance();
+            $conn = $connectdb->getConnection();
+            if(!empty($id) AND !empty($mail) AND !empty($pass)){
+              
+                $sqlmember ="SELECT Member_id and Member_email and Member_password FROM member WHERE Member_id='$id' and Member_email = '$mail' and Member_password = '$pass'";
+                $sqltrainer ="SELECT Trainer_id and Trainer_email and Trainer_password FROM trainer WHERE Trainer_id='$id' and Trainer_email = '$mail' and Trainer_password = '$pass'";
+                $sqladmin="SELECT Admin_id and Admin_email and Admin_password FROM admin WHERE Admin_id='$id' and Admin_email = '$mail' and Admin_password = '$pass'";
+                if(isset($sqltrainer)){
+                    $result= mysqli_query($conn,$sqltrainer);
+                    if(mysqli_num_rows($result)==1){
+                        session_start();
+                        $_SESSION['id']=$id;
+                        $_SESSION['mail']=$mail;
+                        $_SESSION['pass']=$pass;
+                         header('location:indexTrainer.php'); 
+                          
+                         } 
+     
+                     }
+                if(isset($sqlmember)){
+                        $result= mysqli_query($conn,$sqlmember);
+                        if(mysqli_num_rows($result)==1){
+                            session_start();
+                            $_SESSION['id']=$id;
+                            $_SESSION['mail']=$mail;
+                            $_SESSION['pass']=$pass;
+                             header('location:indexMember.php'); 
+                              
+                             } 
+         
+                    }
+                    if(isset($sqladmin)){
+                        $result= mysqli_query($conn,$sqladmin);
+                        if(mysqli_num_rows($result)==1){
+                            session_start();
+                            $_SESSION['id']=$id;
+                            $_SESSION['mail']=$mail;
+                            $_SESSION['pass']=$pass;
+                             header('location:indexAdmin.php'); 
+                              
+                             } 
+                             else{
+                                 echo "email or password are wrong";
+                            }
+         
+                    }
+                }
+                else{
+                    echo "Please enter email and password";
+                }
+
+            
+           
+        }
+        function logout(){
+            $connectdb  = connectdb::getInstance();
+            $conn = $connectdb->getConnection();
+            session_start();
+            session_destroy();
+            unset($_SESSION['mail']);
+
+            header('location:index1.php');     
+          
+        }
+        function updateProfile($oldpass,$newpass,$confirmpass){
+            $connectdb  = connectdb::getInstance();
+            $conn = $connectdb->getConnection();
+            $id=$_SESSION['id'];
+            if(isset($id)){
+                $query="SELECT Member_password FROM Member WHERE Member_id='$id'";
+                $query2="SELECT Trainer_password FROM Trainer WHERE Trainer_id='$id'";
+                if(isset($query)){
+                    $result= mysqli_query($conn,$query);
+                    while($row=mysqli_fetch_array($result)){
+                        $pass=$row['Member_password'];
+                        if($pass==$oldpass){
+                            if($newpass==$confirmpass){
+                                $updatequery="UPDATE Member SET Member_password='$confirmpass' WHERE Member_id='$id'";
+                                $update=mysqli_query($conn,$updatequery);
+                                echo"updated successfully";
+                            }
+                            else{
+                                echo"passwords don't match";
+                            }
+
+                        }
+                    }
+
+                }
+                if(isset($query2)){
+                    $result= mysqli_query($conn,$query2);
+                    while($row=mysqli_fetch_array($result)){
+                        $pass=$row['Trainer_password'];
+                        if($pass==$oldpass){
+                            if($newpass==$confirmpass){
+                                $updatequery="UPDATE Trainer SET Trainer_password='$confirmpass' WHERE Trainer_id='$id'";
+                                $update=mysqli_query($conn,$updatequery);
+                                echo"updated successfully";
+                            }
+                            else{
+                                echo"passwords don't match";
+                            }
+
+                        }
+                    }
+
+                }
+            }
+            else{
+                header('location:signinform.php');   
+            }
+
+
+        }
+        function signup($fname,$lname,$email,$password,$number){
+            $connectdb  = connectdb::getInstance();
+            $conn = $connectdb->getConnection();
+            if(!empty($fname) AND !empty($lname) AND !empty($email) AND !empty($password) AND !empty($number)){
+              
+                $sql ="INSERT INTO Member(Member_Fname,Member_Lname,Member_email,Member_password,MobileNumber) VALUES ('$fname','$lname','$email','$password','$number')";
+                $result= mysqli_query($conn,$sql);
+                   $_SESSION['message']="welcome";
+                   $_SESSION['id']=$id;
+                   $_SESSION['fname']=$fname;
+                   $_SESSION['email']=$email;
+                    header('location:indexMember.php');  
+                }
+                else{
+                    echo "Please fill the data";
+                }
+        }
+
+
+
+    }
+ 
+    $personobject= new person();
+
+    if(isset($_POST['loginbtn'])){
+
+        $personobject->login($_POST['id'],$_POST['mail'],$_POST['pass']);
+    }
+    if(isset($_POST['logoutbtn'])){
+        $personobject->logout();
+    }
+    if(isset($_POST['changepassbtn'])){
+        $personobject->updateProfile($_POST['oldpass'],$_POST['newpass'],$_POST['confirmpass']);
+       
+    }
+    if(isset($_POST['signupbtn'])){
+        $personobject->signup($_POST['fname'],$_POST['lname'],$_POST['email'],$_POST['password'],$_POST['number']);
+       
+    }
+ 
+    
+
+
+?>
---
+++ Project_Design/indexAdmin.php
@@ -0,0 +1,147 @@
+<?php
+   // include('..//ApplicationLayer/PersonClass.php');
+    include('../ApplicationLayer/AdminClass.php');
+
+    if(isset($_POST['report']))
+    {
+        $admin = new Admin(1);
+
+        //   $get_parameters="action=view & file = indexAdmin.pdf";
+    //   echo "<a href='indexAdmin.php?$get_parameters' target='_blank'></a>";
+        $admin->generatePdf();
+        //echo "Report button done <br>";
+    }
+    
+?>
+
+<html>
+    <head>
+        <title>FitnessHouse.com</title>
+        <link href="css/bootstrap.min.css " rel="stylesheet">
+        <link href="file.css" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:700" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+        <link href="css/fontawesome-free-5.8.1-web/css/all.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+
+    </head>
+    <body>
+       <div class="home" >
+            <div class="nav">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark nav">
+                    <a href="#"><img src="images/logo3.jpg" class="navimg"></a>
+                        <a href="#" class="coder">Fitness House</a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse " id="navbarNav">
+                            <ul class="navbar-nav">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="<?php echo 'index1.php'; ?>">HOME <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="<?php echo 'index1.php #aboutus'; ?>">ABOUT US</a></li>
+                                <li class="nav-item">                        
+                                    <div class="dropdown">
+                                        <a onclick="myFunction()" class="dropbtn nav-link text-white" >MORE<img src="images/arrow.jpg" class="arrow"> </a>
+                                        <div id="myDropdown" class="dropdown-content">
+                                            <a href="<?php echo 'index1.php #sessions'; ?>">Sessions</a>
+                                            <a href="<?php echo 'index1.php #packages'; ?>">Packages</a>
+                                            <a href="<?php echo 'index1.php #branches'; ?>">Our Branches</a>
+                                            <a href="<?php echo 'index1.php #contactus'; ?>">Contact Us</a>
+                                        </div>
+                                    </div>
+                                </li>
+                                  
+                                <li class="nav-item">
+                                    <a class="nav-link text-white" href="<?php echo 'indexAdmin.php'; ?>"><i class="fas fa-user-circle text-white width:10%"></i> MANAGE USERS</a>
+                                        
+                                </li>
+                                <li class="nav-item">
+                                    <form method="POST">
+                                        <a class="nav-link " ><button class="bg-dark text-white" name="logoutbtn">LOGOUT</button> <i class="fas fa-long-arrow-alt-right text-white"></i></a>
+                                    </form>    
+                                </li>
+                            </ul>
+                        </div>
+                </nav>
+           </div>
+           <div class="overlay ">
+                <div class="container middle text-white choose">
+                    <a href="<?php echo 'memberModifications.php'; ?>" class="pick_btn middle btn btn_inverse cover_cta scroll text-white ">
+                        <button class="text-white p-2 px-4 button "> <h1 class="m-5">Member »</h1></button>
+                    </a>
+                    <a href="<?php echo 'trainerModifications.php'; ?>" class=" pick_btn middle btn btn_inverse cover_cta scroll text-white ">
+                        <button class="text-white p-2 px-4 button "> <h1 class="m-5">Trainer »</h1></button>
+                    </a>
+                    <a href="<?php echo 'packagesModifications.php'; ?>" class="pick_btn middle btn btn_inverse cover_cta scroll text-white ">
+                        <button class="text-white p-2 px-4 button "> <h1 class="m-5">Packages »</h1></button>
+                    </a>
+                    <a href="<?php echo 'sessionsModifications.php'; ?>" class=" pick_btn middle btn btn_inverse cover_cta scroll text-white ">
+                        <button class="text-white p-2 px-4 button "> <h1 class="m-5">Sessions »</h1></button>
+                    </a>
+
+                    <form action = 
+                        
+                   " <?php echo $_SERVER['PHP_SELF'] ?> " method = "POST">
+                           <input type="submit" name="report" value="Report"  
+                           class="text-white bg-dark p-4">
+ 
+                     </form>
+                </div>
+                        
+            </div>
+       </div>
+       <section id="review"> 
+            <div class="container">
+                <div class="row" >
+                    <div class="secondiv text-white">
+                        <h1 class="ftitle text-white">REVIEWS</h1>
+                        <br>
+                        <div class="container reviewtext">
+                            <h2>Ahmed Waleed</h2>
+                            <br>
+                            <p>very good coach he helped me get in shape in no time at all</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+       <script>
+           /* When the user clicks on the button, 
+              toggle between hiding and showing the dropdown content */
+            function myFunction() {
+                 document.getElementById("myDropdown").classList.toggle("show");
+            }
+                        
+             // Close the dropdown if the user clicks outside of it
+            window.onclick = function(event) {
+                 if (!event.target.matches('.dropbtn')) {
+                      var dropdowns = document.getElementsByClassName("dropdown-content");
+                      var i;
+                     for (i = 0; i < dropdowns.length; i++) {
+                          var openDropdown = dropdowns[i];
+                         if (openDropdown.classList.contains('show')) {
+                             openDropdown.classList.remove('show');
+                            }
+                         }
+                     }
+                 }
+        </script>
+            
+        <script src="js/jquery.min.js"></script>
+        <script src="js/popper.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+
+
+    </body>
+
+</html>
